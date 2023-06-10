12eda57d9d2b5baf2ffcef51557bab7e634fec4e
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-10 23:25:07 +0600

    Did all instructor and all classes

--- index.js
+++ index.js
@@ -40,6 +40,16 @@ async function run() {
             const result = await instructorCollection.find().limit(6).toArray();
             res.send(result);
         })
+
+        app.get('/instructors', async (req, res) => {
+            const result = await instructorCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/classes', async (req, res) => {
+            const result = await classCollection.find().toArray();
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
