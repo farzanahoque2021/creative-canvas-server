158c7d2c28c4f24498e3603287773268dc02ab23
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-12 12:51:47 +0600

    Did delete method for carts

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yj4hdna.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -65,6 +65,13 @@ async function run() {
             }
             const query = { email: email }
             const result = await cartCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
             res.send(result)
         })
         // Send a ping to confirm a successful connection
